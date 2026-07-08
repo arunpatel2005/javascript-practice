cab58e645c781f7921c44a89074932672393f125
Arun Patel <arunpatel174986@gmail.com>
2026-07-08 19:40:20 +0530

    Added loops

---
+++ loops/loops.js
@@ -0,0 +1,7 @@
+let GameNumber=25;
+let UserNumber=prompt("guess a numer to win the game ");
+
+while(UserNumber!=GameNumber){
+    UserNumber=prompt("guess a another numer to win the game ");
+}
+console.log("congratulations you won the game");
